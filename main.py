7105659b995abc0164602dca8f40755d340e74e6
Oleg Malovitsky <oleg.malovitsky@quantfury.com>
2021-09-22 21:20:40 +0300

    delete comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import flask
 import faker
 import datetime
 import random
-from flask import Flask, render_template
+from flask import Flask
 import string
 from flask import request
 from marshmallow import validate
@@ -27,70 +27,7 @@ def hello():
 @app.route("/now")
 def get_current_time():
     return str(datetime.datetime.now())
-@app.route("/password")
-@use_kwargs({
-    "length": fields.Int(
-        required=True,
 
-        validate=[validate.Range(min=1, max=100)]
-    ),
-    "specials": fields.Int(
-        required=False,
-        validate=[validate.OneOf([0, 1])]
-    ),
-    "digits": fields.Int(
-        required=False,
-        validate=[validate.OneOf([0, 1])]
-    )},
-    location="query"
-)
-def pass_gen(**kwargs):
-    specials = kwargs.get('specials', False)
-    digits = kwargs.get('digits', False)
-
-    random_base = string.ascii_lowercase + string.ascii_uppercase
-
-    if specials:
-        random_base += string.punctuation
-        return ''.join(random.choices(random_base, k=kwargs['length']))
-    if digits:
-        random_base += string.digits
-        return ''.join(random.choices(random_base, k=kwargs['length']))
-    else:
-        return "".join(random.choices(random_base, k=kwargs['length']))
-
-
-@app.route("/bitcoin_rate")
-@use_kwargs({
-    "currency": fields.Str(
-        required=False,
-
-        validate=[validate.ContainsOnly("UAH")]
-    )
-},
-    location="query"
-)
-def get_bitcoin_rate(**kwargs):
-    currency = kwargs.get('currency')
-    print(currency)
-    headers = {
-        'content-type': 'application/json;charset=utf-8',
-        'User-Agent': 'Python'
-
-    }
-
-    url = "https://bitpay.com/api/rates"
-
-    request_to_url = requests.get(url, headers=headers)
-
-    if currency:
-        for element in request_to_url.json():
-            if element["code"] == currency:
-                return render_template('index_currency.html', name=str(element))
-    else:
-        for element in request_to_url.json():
-            if element["code"] == "USD":
-                return render_template('index_currency.html', name=str(element))
 
 
 @app.route("/get_random_students")
@@ -107,8 +44,7 @@ def get_pipenv():
         e = file.read()
         return e
 e = string
-#print(dir(e))
 sequens = [string.punctuation+string.ascii_lowercase]
-#print(sequens)
+
 
 app.run(debug=True)
